4428a37d399540fad5ab869784c6e788c47e29df
Arjun Neema <arjunneema0984@gmail.com>
2025-04-25 10:46:06 +0530

    Delete NBA4.R

--- NBA4.R
+++
@@ -1,6 +0,0 @@
-library(ggplot2)
-library(corrplot)
-library(dplyr)
-numeric_vars <- df %>% select(where(is.numeric))  # Select only numeric columns
-corr_matrix <- cor(numeric_vars, use="complete.obs")
-corrplot(corr_matrix, method="color", type="upper", tl.col="black", tl.srt=45)
